42bb35ab6b3c4598ec0efc4d9cc361ebe884d866
Adrian Gallant <adrian.gallant@student.fairfield.edu>
2020-10-06 23:33:24 -0400

    update

--- Homework/HW2/PatientCRUDHw/src/main/java/com/PatientCRUDHw/repositories/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.PatientCRUDHw.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.PatientCRUDHw.models.Patient;
-
-public interface PatientRepository extends CrudRepository<Patient, Integer> {
-
-}
