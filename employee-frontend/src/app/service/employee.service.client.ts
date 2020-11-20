e7dd1f3265557abeda9e206ffd7aadf88120dbab
Adrian Gallant <adrian.gallant@student.fairfield.edu>
2020-11-19 19:13:37 -0500

    changed url

--- employee-frontend/src/app/service/employee.service.client.ts
+++ employee-frontend/src/app/service/employee.service.client.ts
@@ -3,5 +3,5 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class EmployeeServiceClient{
   getAllEmployees = () =>
-    fetch('http://localhost:8080/api/v1/getAllEmployees').then(response => response.json())
+    fetch('http://employeebackend-env.eba-7npijpzr.us-east-2.elasticbeanstalk.com/api/v1/getAllEmployees').then(response => response.json())
 }
