d81aebb6fc28f0756dd18b7457edd3a28153f653
Adrian Gallant <adrian.gallant@student.fairfield.edu>
2020-11-19 19:43:08 -0500

    added a comment

--- employee-frontend/src/app/service/employee.service.client.ts
+++ employee-frontend/src/app/service/employee.service.client.ts
@@ -5,3 +5,4 @@ export class EmployeeServiceClient{
   getAllEmployees = () =>
     fetch('http://employeebackend-env.eba-7npijpzr.us-east-2.elasticbeanstalk.com/api/v1/getAllEmployees').then(response => response.json())
 }
+// http://employeebackend-env.eba-7npijpzr.us-east-2.elasticbeanstalk.com/api/v1/getAllEmployees
