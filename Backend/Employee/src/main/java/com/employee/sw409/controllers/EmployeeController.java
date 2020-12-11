e1007fb28701def1ffbd8606a62f11831b24f5f9
Adrian Gallant <adrian.gallant@student.fairfield.edu>
2020-12-10 19:33:08 -0500

    lab

--- Backend/Employee/src/main/java/com/employee/sw409/controllers/EmployeeController.java
+++ Backend/Employee/src/main/java/com/employee/sw409/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import com.employee.sw409.services.EmployeeService;
 
 @RestController
 @CrossOrigin("*")
+@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
 public class EmployeeController {
 	//https://d2qapc7p0tvi6g.cloudfront.net/
 	@Autowired
